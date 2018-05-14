955aee3c278bf757a31ad0c9293a5da464c1e85d
Ulric Ye <yye90012@gmail.com>
2018-05-13 21:57:49 -0700

    Finished setting up login and signup data stuff

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,21 @@ $(document).ready(() => {
       
       database.ref('users/').once('value', (snapshot) => {
         const data = snapshot.val();
-        console.log('data: ',data);
+        const keys = Object.keys(data);
+        const uname = $('#usernameinput').val();
+        const psd = $('#passwordinput').val();
+
+        if(keys.includes(uname)) {
+            if(data[uname].password == psd) {
+                window.location.href = '/index';
+            } else {
+                $('#status').html('');
+                $('#status').append('Wrong password!');
+            }
+        } else {
+            $('#status').html('');
+            $('#status').append('Wrong username!');
+        }
       });
 
       //database.ref('users/').remove();
