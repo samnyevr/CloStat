b1b02d80e3330c6464626108496a6bdb9484a512
Ulric Ye <yye90012@gmail.com>
2018-05-15 15:50:22 -0700

    Added temperature switching from F to C

---
+++ public/js/database.js
@@ -0,0 +1,10 @@
+    // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA9qeDEWivwl8pN0OjpKABOS8bOZZHZiOw",
+    authDomain: "clothstat.firebaseapp.com",
+    databaseURL: "https://clothstat.firebaseio.com",
+    projectId: "clothstat",
+    storageBucket: "clothstat.appspot.com",
+    messagingSenderId: "692402704354"
+    };
+firebase.initializeApp(config);
