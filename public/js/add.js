3c6c4c1c31d12aa78b3589d5165df2f9e09a31b1
Lixiang Zhou <liz106@ucsd.edu>
2018-05-15 21:48:28 -0700

    add page

--- public/js/add.js
+++ public/js/add.js
@@ -3,7 +3,17 @@ $(document).ready(() => {
         const database = firebase.database();
         database.ref(`users/`).once('value', (snapshot) => {
             const data = snapshot.val();
-          
+            const name = $('#name').val();
+            console.log(name);
+            const type =$('#type').val();
+            console.log(type);
+            // console.log(`users/Bob/Clothes/${type}/${name}`);
+            database.ref(`users/Bob/Clothes/${type}/${name}`).set({
+              clean: true,
+              numberUsage: 0,
+              temp: $('#temperature').val(),
+              // photo: $('#photo').val()
         });
     });
 });
+})
