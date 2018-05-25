84b5578ccfc922e09468001f88fb0bef54fdb31e
Lixiang Zhou <liz106@ucsd.edu>
2018-05-25 10:03:54 -0700

    hello

--- public/js/bottom.js
+++ public/js/bottom.js
@@ -11,12 +11,15 @@ $(document).ready(() =>{
     for(const clothes of bottomKey) {
 
 
-      $('.panel-group').append(` <div class="panel panel-default"> <div class="panel-heading"><p class="title" data-toggle="collapse" data-parent="#accordion" href="#collapse${number}"> ${clothes}</p></div>
+      $('.panel-group').append(` <div class="panel panel-default"> <div class="panel-heading">
+      <p class="title" data-toggle="collapse" data-parent="#accordion" href="#collapse${number}"> ${clothes}</p></div>
       <div class="round">
     					<input type="checkbox" id="checkbox" />
     					<label for="checkbox"></label>
   					</div>
-      <div id="collapse${number}" class="panel-collapse collapse in"> <div class="panel-body"> <img class="pic" width="120" src="/images/lightBlueJeans.jpeg"><p class="words">You have worn this shirt 3 times this month. </p></div></div></div>`);
+      <div id="collapse${number}" class="panel-collapse collapse in"> <div class="panel-body">
+      <img class="pic" width="120" src="/images/lightBlueJeans.jpeg">
+      <p class="words">You have worn this shirt 3 times this month. </p></div></div></div>`);
       number=number+1;
 
     }
